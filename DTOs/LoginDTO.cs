de58ad74ae680033c5b7c016f24a8c1d9253bd9d
Devrim Savas <137702797+devrimsavas@users.noreply.github.com>
2025-09-13 02:49:27 +0200

    removed

--- DTOs/LoginDTO.cs
+++ DTOs/LoginDTO.cs
@@ -1,5 +1,5 @@
 public class LoginDTO
 {
-    public string Username { get; set; }
-    public string Password { get; set; }
+    public string Username { get; set; } = String.Empty;
+    public string Password { get; set; } = String.Empty;
 }
